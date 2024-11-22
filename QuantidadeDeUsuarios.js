8f134d5b760a32ea92676a7128664daf2fc4504f
nanda158 <117355312+nanda158@users.noreply.github.com>
2024-11-22 13:01:44 -0300

    Update QuantidadeDeUsuarios.js

--- QuantidadeDeUsuarios.js
+++ QuantidadeDeUsuarios.js
@@ -1,1 +1,40 @@
+async function quantidadeUsuarios() {
 
+}
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+  }
+  async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+  }
+  async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+  }
+  
+  quantidadeUsuarios()
+  const data = [
+    {
+      x: 'nomeDasRedes',
+      y: quantidadeDeUsuarios,
+      type: 'bar',
+      marker: {
+        color: 'red'
+      }
+    }
+  ]
+  // código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo'
+    }
+  }
+  import { getCSS, tickConfig } from "./common.js"
+
+
+  // código omitido
